e855ebd2dcd3e203a1500f35528fb9fe3ad3a6cf
Miguel Hernandez Martos <enlavin@enlavin.com>
2015-05-24 11:10:24 +0200

    power off screen

--- webapp.py
+++ webapp.py
@@ -6,15 +6,19 @@ app = Flask(__name__)
 api = Api(app)
 tvremote = TVRemote(device='/dev/ttyUSB0')
 
-volumedir_parser = reqparse.RequestParser()
-volumedir_parser.add_argument('dir', str)
+action_parser = reqparse.RequestParser()
+action_parser.add_argument('action', str)
 
 volume_parser = reqparse.RequestParser()
 volume_parser.add_argument('level', int)
 
 
-
 class TVVolume(Resource):
+    VOLUME_ACTIONS = ['up', 'down', 'mute', 'normal']
+
+    def _isvalid_volume_action(self, action):
+        return action in TVVolume.VOLUME_ACTIONS
+
     def put(self):
         args = volume_parser.parse_args()
         level = int(args['level'])
@@ -23,21 +27,44 @@ class TVVolume(Resource):
 
         tvremote.cmd_volume_set(int(args['level']))
 
-    def post(self):
-        args = volumedir_parser.parse_args()
-        if args['dir'] not in ['up', 'down', 'mute']:
-            raise ValueError('"dir" param should be up/down/mute, not "{}"'.format(args['dir']))
+        return {'level': level}, 200
 
-        if args['dir'] == 'up':
+    def post(self):
+        args = action_parser.parse_args()
+        action = args['action']
+        if not self._isvalid_volume_action(action):
+            raise ValueError('"action" param should be up/down/mute, not "{}"'.format(action))
+
+        if action == 'up':
             tvremote.cmd_volume_up()
-        elif args['dir'] == 'down':
+        elif action == 'down':
             tvremote.cmd_volume_down()
+        elif action == 'normal':
+            tvremote.cmd_volume_set(12)
         else:
-            tvremote.cmd_volume_mute()
+            tvremote.cmd_volume_set(0)
+
+        return {}, 204
+
+
+class TVScreen(Resource):
+    def _isvalid_action(self, action):
+        return action == 'off'
+
+    def post(self):
+        args = action_parser.parse_args()
+        action = args['action']
+        if not self._isvalid_action(action):
+            raise ValueError('"action" should be "off"')
+
+        tvremote.cmd_power_off()
+
+        return {}, 204
 
 
 
 api.add_resource(TVVolume, '/tv/volume')
+api.add_resource(TVScreen, '/tv/screen')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
